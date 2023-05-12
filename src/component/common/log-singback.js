753d7ac9b2149a28e7517f55ac75545aa29e9fec
snowflaver <sirayukiyui@gmail.com>
2023-05-12 22:59:23 +0900

    로그인 화면
    로그인 화면을 중앙으로 모으는 시도

--- src/component/common/log-singback.js
+++ src/component/common/log-singback.js
@@ -4,7 +4,8 @@ const Backarea = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-justify-content: center;
+justify-content: flex-start;
+padding-top: 20px;
 `
 
 export { Backarea }

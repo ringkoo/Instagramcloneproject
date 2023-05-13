dbc86004b0102fea2a1f1290de215d4196e4c250
snowflaver <sirayukiyui@gmail.com>
2023-05-13 12:13:04 +0900

    api 추가
    회원 목록 조회
    팔로우
    팔로우 조회
    팔로잉 조회

--- src/api/users.js
+++ src/api/users.js
@@ -1,23 +1,70 @@
 import axios from "axios";
 
 //회원가입
-const signupPost = async({ email, nickname, password }) => {
+const signupPost = async ({ email, nickname, password }) => {
     const res = await axios.post(
-        `${process.env.SERVER_URL}/members/signup`,{
-            email, nickname, password
-        }
+        `${process.env.SERVER_URL}/members/signup`, {
+        email, nickname, password
+    }
     )
     return res.data
 }
 
 //로그인
-const loginPost = async({email, password}) => {
+const loginPost = async ({ email, password }) => {
     const res = await axios.post(
-        `${process.env.SERVER_URL}/members/login`,{
-            email, password
-        }
+        `${process.env.SERVER_URL}/members/login`, {
+        email, password
+    }
     )
     return res.data
 }
 
-export { loginPost, signupPost}
+//유저 조회
+const userInquiry = async ({ jwt }) => {
+    const res = await axios.get(
+        `${process.env.SERVER_URL}/members}`, {
+        headers: {
+            Authorization: `Bearer ${jwt}`
+        }
+    }
+    )
+    return res.data
+}
+
+//팔로우 상태
+const followPost = async ({ userId, jwt }) => {
+    const res = await axios.post(
+        `${process.env.SERVER_URL}/members/${userId}`, {
+        headers: {
+            Authorization: `Bearer ${jwt}`
+        }
+    }
+    )
+    return res.data
+}
+//팔로우 조회
+const followInquiry = async ({ userId, jwt }) => {
+    const res = await axios.get(
+        `${process.env.SERVER_URL}/members/${userId}`, {
+        headers: {
+            Authorization: `Bearer ${jwt}`
+        }
+    }
+    )
+    return res.data
+}
+
+//팔로잉 조회
+const followingInqury = async ({ userId, jwt }) => {
+    const res = await axios.get(
+        `${process.env.SERVER_URL}/members/${userId}`, {
+        headers: {
+            Authorization: `Bearer ${jwt}`
+        }
+    }
+    )
+    return res.data
+}
+
+export { loginPost, signupPost, userInquiry, followPost, followInquiry, followingInqury }
